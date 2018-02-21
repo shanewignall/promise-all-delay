aa9cdf892262aa0264e4eed17fc2542269f37f1d
Shane Wignall <shane.wignall@gmail.com>
2018-02-21 16:39:31 -0600

    Update promiseAll.js
    replaced pop with shift (oops)

--- promiseAll.js
+++ promiseAll.js
@@ -9,7 +9,7 @@ function promiseAll(promises, delay) {
       promises[0]()
         .then(() => {
           setTimeout(() => {
-            promises.pop();
+            promises.shift();
 
             if (promises.length) {
               doPromise();

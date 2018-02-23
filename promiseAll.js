a5e5a9b1289bd1bb14e14a7b879ffd6e33d117bc
Shane Wignall <shane.wignall@gmail.com>
2018-02-23 12:02:53 -0600

    Update promiseAll.js

--- promiseAll.js
+++ promiseAll.js
@@ -4,6 +4,7 @@
  * @param {integer} delay - An amount of time to wait between each promise execution in ms.
  * @param {integer} concurrency - The max number of parallel promises
  */
+
 function promiseAll(promises, delay, concurrency) {
   return new Promise((resolve, reject) => {
     var batches = [];

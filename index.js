28c577803d8a725a9dcdbf0eceaa3dd74d1dcd90
Shane Wignall <shane.wignall@gmail.com>
2019-01-10 12:07:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Executes an array of promise-returning functions
+ * sequentially
+ * 
+ * @param {Function[]} list 
+ *      Array of promise-returning functions
+ */
 function pSeries(list) {
     let p = Promise.resolve();
 
